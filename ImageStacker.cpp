72d85d3b6219af2c18bf6f1414984cf372ae3171
StonedCat <matgaust@ut.utm.edu>
2016-09-27 12:17:41 -0500

    Update ImageStacker.cpp

--- ImageStacker.cpp
+++ ImageStacker.cpp
@@ -1,4 +1,4 @@
-/* Author: Stephen Stinson
+/* Author: Stephen Stinson/Matthew Austin
  * Date: 9/22/16
  * Description: The implementation file for the header file.
  */
@@ -28,13 +28,22 @@ void stacker::loadFile(string file){
   ifstream new_input(file.c_str());
 
  new_input >> magic_number >> width >> height >> max_color;
+ int w = 0;
   for(int i = 0; i < width; i++)
   {
     for(int j = 0; j < height; j++)
       {
+	
 	pixel Pixel;
 	new_input >> Pixel.red >> Pixel.green >> Pixel.blue;
 	pixels.push_back(Pixel);
+	pixels[w].red = Pixel.red;
+	pixels[w].green = Pixel.green;
+	pixels[w].blue = Pixel.blue;
+	cout << Pixel.red << Pixel.green << Pixel.blue << endl;//these printouts aren"t working
+	cout << pixels[w].red << pixels[w].green << pixels[w].blue << endl;
+	w++;
       }
+    
   }
-}
+}	
