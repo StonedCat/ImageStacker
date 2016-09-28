6843cf8e4a0ccc23ebb86ee86b50429b7eed989b
StonedCat <matgaust@ut.utm.edu>
2016-09-28 15:07:49 -0500

    Update ImageStacker.cpp

--- ImageStacker.cpp
+++ ImageStacker.cpp
@@ -36,14 +36,65 @@ void stacker::loadFile(string file){
 	
 	pixel Pixel;
 	new_input >> Pixel.red >> Pixel.green >> Pixel.blue;
-	pixels.push_back(Pixel);
-	pixels[w].red = Pixel.red;
-	pixels[w].green = Pixel.green;
-	pixels[w].blue = Pixel.blue;
-	cout << Pixel.red << Pixel.green << Pixel.blue << endl;//these printouts aren"t working
-	cout << pixels[w].red << pixels[w].green << pixels[w].blue << endl;
+	 pixels.push_back(Pixel);
+	  pixels[w].red = Pixel.red;
+	 pixels[w].green = Pixel.green;
+	 pixels[w].blue = Pixel.blue;
+	 //cout << Pixel.red << Pixel.green << Pixel.blue << endl;
+	// //	cout << pixels[w].red << pixels[w].green << pixels[w].blue << endl;
 	w++;
       }
     
   }
 }	
+int stacker::get_width(){
+  return  width;
+}
+int stacker::get_height(){
+  return  height;
+}
+
+  
+stacker stacker::ave_stack(stacker s){
+  stacker New;
+  int w = 0;
+  for(int i = 0; i < width; i++)
+  {
+    for(int j = 0; j < height; j++)
+      {
+
+	New.pixels[w].red = (pixels[w].red +s.pixels[w].red)/2;
+	New.pixels[w].green = (pixels[w].green +s.pixels[w].green)/2;
+	New.pixels[w].red = (pixels[w].blue +s.pixels[w].blue)/2;
+      }
+  }
+  return New;
+}
+stacker&  stacker::operator=(stacker LHS){
+  int w = 0;
+  for(int i = 0; i < width; i++)
+  {
+    for(int j = 0; j < height; j++)
+      {
+	LHS.pixels[w].red = pixels[w].red;
+	LHS.pixels[w].green = pixels[w].green;
+	LHS.pixels[w].blue = pixels[w].blue;
+	w++;
+      }
+  }
+  return LHS;
+}
+void stacker::Writefile(string file){
+   ofstream new_output(file.c_str());
+
+   new_output << magic_number << "\n" << width  << " " << height << "\n" << max_color << "\n";
+ int w = 0;
+  for(int i = 0; i < width; i++)
+  {
+    for(int j = 0; j < height; j++)
+      {
+	new_output << pixels[w].red << " " << pixels[w].green << " " << pixels[w].blue  << "\n";
+	w++;
+      }
+    
+  }
