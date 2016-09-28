88704bc31f3d98b0716256604d741bc146d3b463
StonedCat <matgaust@ut.utm.edu>
2016-09-28 15:08:46 -0500

    Update ImageStacker.h

--- ImageStacker.h
+++ ImageStacker.h
@@ -1,4 +1,4 @@
-/*Author: Stephen Stinson
+/*Author: Stephen Stinson/Matthew Austin
  *Date: 9/22/16
  *Description: header file for the class stacker.
  */
@@ -19,17 +19,22 @@ class stacker{
   int height; // the image height;
   int max_color;// the maximum color depth (this assignment it will be 255)
   
-  struct pixel{
+   struct pixel{
     int red;
     int green;
     int blue;
-  };// this will be the struct containing the R, G, B values.
-  vector <pixel> pixels;//a Vector of pixel structs storing color info. for the image.
+     };// this will be the struct containing the R, G, B values.
+   vector <pixel> pixels;//a Vector of pixel structs storing color info. for the image.
+   // pixel *pixels;
 public:
+  void Writefile(string file);
+  stacker& operator=(stacker LHS);
+  stacker ave_stack(stacker s);
+  int get_width();
+  int get_height();
   stacker();
   void loadFile(string file);
   // void  vector<pixel> push(pixel n);
-
 };
 
 
